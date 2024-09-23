58fe338ebaf27c8f852c75f6482e58e9c4f294b1
tomtitherington <tom.titherington@gmail.com>
2024-09-23 18:44:15 +0100

    feat(toolbar): show tooltip when comment mode is on

--- packages/ui/src/toolbar.tsx
+++ packages/ui/src/toolbar.tsx
@@ -126,27 +126,48 @@ const Toolbar = ({ className }: ToolbarProps): JSX.Element => {
     <>
       <div
         className={cn(
-          'absolute bottom-4 left-1/2 flex -translate-x-1/2 transform items-center space-x-1 rounded-md border bg-background p-1 shadow-sm',
+          'absolute bottom-4 left-1/2 flex -translate-x-1/2 transform flex-col items-center justify-center space-y-2',
           className
         )}
         ref={toolbarRef}
       >
-        <ToggleGroup.Root
-          disabled={isCommenting}
-          onValueChange={(newValue) => {
-            setMenuValue(newValue);
-          }}
-          type="single"
-          value={menuValue}
+        <div
+          className={cn(
+            'rounded-md border-[0.5px] bg-primary p-1 px-2 shadow-sm animate-in fade-in-0 zoom-in-95 data-[state=closed]:hidden data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95'
+          )}
+          data-state={isCommenting ? 'open' : 'closed'}
         >
-          <ToggleGroupItem value="comment" variant="default">
-            <MessageCircle className="h-5 w-5" />
-          </ToggleGroupItem>
+          <p className={cn('text-sm text-primary-foreground')}>
+            Press{' '}
+            <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border-[0.5px] bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+              ESC
+            </kbd>{' '}
+            to dismiss.
+          </p>
+        </div>
 
-          <ToggleGroupItem value="inbox" variant="default">
-            <Inbox className="h-5 w-5" />
-          </ToggleGroupItem>
-        </ToggleGroup.Root>
+        <div
+          className={cn(
+            'flex items-center space-x-1 rounded-md border bg-background p-1 shadow-sm'
+          )}
+        >
+          <ToggleGroup.Root
+            disabled={isCommenting}
+            onValueChange={(newValue) => {
+              setMenuValue(newValue);
+            }}
+            type="single"
+            value={menuValue}
+          >
+            <ToggleGroupItem value="comment" variant="default">
+              <MessageCircle className="h-5 w-5" />
+            </ToggleGroupItem>
+
+            <ToggleGroupItem value="inbox" variant="default">
+              <Inbox className="h-5 w-5" />
+            </ToggleGroupItem>
+          </ToggleGroup.Root>
+        </div>
       </div>
       {isCommenting && (
         <div
